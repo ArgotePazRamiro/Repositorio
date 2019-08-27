f1c74c613f3dbb5bb993621ac73411f977557cfd
ArgotePazRamiro <argote_paz_ramiro@hotmail.com>
2019-08-27 17:52:38 -0400

    update

--- asignaciones/asignacion9/javascript.js
+++ asignaciones/asignacion9/javascript.js
@@ -5,20 +5,19 @@ request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
 request.onload = function () {
-    var superHeroes = request.response;
-    populateHeader(superHeroes);
-    showHeroes(superHeroes);
+    var recomendados = request.response;
+    populateHeader(recomendados);
 }
 function populateHeader(jsonObj) {
     var myH1 = document.createElement('h1');
     myH1.textContent = jsonObj['Lugar'];
-    header.appendChild(myH1);
+    recomendados.appendChild(myH1);
 
     var myH2 = document.createElement('h2');
     myH1.textContent = jsonObj['Municipio'];
-    header.appendChild(myH2);
+    recomendados.appendChild(myH2);
 
     var myPara = document.createElement('p');
     myPara.textContent = 'Descripcion: ' + jsonObj['Descripcion'];
-    header.appendChild(myPara);
+    recomendados.appendChild(myPara);
 }
