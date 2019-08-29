f94d583272d74a9dbb6d32bcf5fc3d52024cecf9
ArgotePazRamiro <argote_paz_ramiro@hotmail.com>
2019-08-29 12:45:12 -0400

    update

--- Llajtatour/js/recomendados.js
+++ Llajtatour/js/recomendados.js
@@ -1,5 +1,10 @@
+var OpenPopUp = document.getElementById("open-popup"),
+    overlay = document.getElementById("overlay"),
+    popup = document.getElementById("popup"),
+    closePopup = document.getElementById("close-popup");
 var requestURL = 'https://github.com/ArgotePazRamiro/Repositorio/blob/master/Llajtatour/json/recomendados.json';
 var request = new XMLHttpRequest();
+var information = document.querySelector('section');
 request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
@@ -8,18 +13,110 @@ request.onload = function(){
     datos(contenidomostrar);
 }
 
-function datos(jsonObj){
-    var contenido = jsonObj['recomendados'];
-    
-    if(contenido[0].Lugar == 'Los Molinos'){
-        var myartculo = document.createElement('h1')
-        var titulociudad = document.createElement('h2');
-        var historiaciudad = document.createElement('p');
 
-        titulociudad.textContent = contenido[0].titulo;
-        historiaciudad.textContent = contenido[0].historia;
+function showplace(jsonObj, a) {
+    var lugares = jsonObj['recomendados'];
+    for (var i = 0; i < lugares.length; i++) {
 
-        myartculo.appendChild(titulociudad);
-        myartculo.appendChild(historiaciudad);
+        if (lugares[i].Lugar == a) {
+            var myArticle = document.createElement('article');
+            var myH1 = document.createElement('h1');
+            var myH2 = document.createElement('h2');
+            var myPara = document.createElement('p');
+
+            myH1.textContent = lugares[i].Lugar;
+            myH2.textContent = lugares[i].Municipio;
+            myPara.textContent = lugares[i].Descripcion;
+        }
+
+        myArticle.appendChild(myH1);
+        myArticle.appendChild(myH2);
+        myArticle.appendChild(myPara);
+
+        information.appendChild(myArticle);
     }
+
 }
+openpopup1.addEventListener('click', function () {
+    var lugar = request.response;
+    request.onload = lugar;
+    overlay.classList.add('active');
+    showplace(lugar, 'Los Molinos');
+});
+openpopup2.addEventListener('click', function () {
+    var lugar = request.response;
+    request.onload = lugar;
+    overlay.classList.add('active');
+    showplace(lugar, 'Laguna Corani');
+});
+openpopup3.addEventListener('click', function () {
+    var lugar = request.response;
+    request.onload = lugar;
+    overlay.classList.add('active');
+    showplace(lugar, 'Puente del Libertador en Mizque');
+});
+openpopup4.addEventListener('click', function () {
+    var lugar = request.response;
+    request.onload = lugar;
+    overlay.classList.add('active');
+    showplace(lugar, 'La Fuente del Amor');
+});
+openpopup5.addEventListener('click', function () {
+    var lugar = request.response;
+    request.onload = lugar;
+    overlay.classList.add('active');
+    showplace(lugar, 'Cavernas del Repechón');
+});
+openpopup6.addEventListener('click', function () {
+    var lugar = request.response;
+    request.onload = lugar;
+    overlay.classList.add('active');
+    showplace(lugar, 'La Angostura');
+});
+openpopup7.addEventListener('click', function () {
+    var lugar = request.response;
+    request.onload = lugar;
+    overlay.classList.add('active');
+    showplace(lugar, 'Convento de San José');
+});
+openpopup8.addEventListener('click', function () {
+    var lugar = request.response;
+    request.onload = lugar;
+    overlay.classList.add('active');
+    showplace(lugar, 'El Tipnis');
+});
+openpopup9.addEventListener('click', function () {
+    var lugar = request.response;
+    request.onload = lugar;
+    overlay.classList.add('active');
+    showplace(lugar, 'Misicuni');
+});
+openpopup10.addEventListener('click', function () {
+    var lugar = request.response;
+    request.onload = lugar;
+    overlay.classList.add('active');
+    showplace(lugar, 'Yunga Pampa');
+});
+openpopup11.addEventListener('click', function () {
+    var lugar = request.response;
+    request.onload = lugar;
+    overlay.classList.add('active');
+    showplace(lugar, 'Parque Ecoturístico San Isidro');
+});
+openpopup12.addEventListener('click', function () {
+    var lugar = request.response;
+    request.onload = lugar;
+    overlay.classList.add('active');
+    showplace(lugar, 'El Cristo de la Concordia');
+});
+openpopup13.addEventListener('click', function () {
+    var lugar = request.response;
+    request.onload = lugar;
+    overlay.classList.add('active');
+    showplace(lugar, 'Parque Nacional Tunari');
+});
+
+closePopup.addEventListener('click', function(){
+    document.querySelector('section').innerHTML='';
+    overlay.classList.remove('active')
+});
